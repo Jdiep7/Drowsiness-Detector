19b27f496ed0aeb0713f10e1ac6e13dba99791f5
Jdiep7 <jdiep705@gmail.com>
2025-05-07 15:36:11 -0400

    Model built and dataset added

--- pytorch.py
+++ pytorch.py
@@ -23,6 +23,18 @@ class EyeDataSet(Dataset):
     @property
     def classes(self):
         return self.data.classes
+    
+class SimpleEyeClassifier(neural.Module):
+    def __init__(self, num_classes=2):
+        super(SimpleEyeClassifier, self).__init__()
+        self.base_model = timm.create_model('efficientnet_b0',pretrained=True)
+        self.features = neural.Sequential(*list(self.base_model.children())[:-1])
+        self.classifier = neural.Linear(self.base_model.classifier.in_features, num_classes)
+    
+    def forward(self, x):
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
 
 
 transform = transforms.Compose([
@@ -33,17 +45,18 @@ transform = transforms.Compose([
 data_dir='C:/Users/jdiep/projects/Drowsiness-Detector/train'
 dataset = EyeDataSet(data_dir, transform)
 
-image, label = dataset[0]
+image, label = dataset[100]
 
-for image,label in dataset:
+for image, label in dataset:
     break
 
 dataLoader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-for image,label in dataLoader:
+for images, labels in dataLoader:
     break
 
-print(dataLoader)
+model = SimpleEyeClassifier(num_classes=2)
+print(model)
 
 
 
